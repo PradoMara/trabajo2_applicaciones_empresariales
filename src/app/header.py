4933b1f2964f8e05b7ff41a0568600a967657edc
Daniel Prado <dprado2024@alu.uct.cl>
2026-04-19 19:38:54 -0400

    cambios

--- src/app/header.py
+++ src/app/header.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 def render_header() -> None:
-	st.title("Registro de Clientes")
+	st.title("Gestión de Clientes")
 	st.caption("Interfaz visual para registrar, actualizar, obtener y eliminar clientes.")
